423254417db3daf352e2f7fc41deba6722c2e3e7
vvasylykha <vvasylykha@chainfulness.com>
2023-11-12 15:19:06 +0200

    update cross config

--- src/main/java/com/athenia/athenia/controller/AuthController.java
+++ src/main/java/com/athenia/athenia/controller/AuthController.java
@@ -19,7 +19,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://ec2-34-227-7-190.compute-1.amazonaws.com", "http://localhost:3000"})
 public class AuthController {
 	@Autowired
 	AuthenticationManager authenticationManager;
