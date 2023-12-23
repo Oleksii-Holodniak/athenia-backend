67254b3221e59ae738ab8426cc03cd228872fd28
Oleksii Holodniak <holodniakwork@gmail.com>
2023-12-23 16:34:45 +0200

    update cors configuration

--- src/main/java/com/athenia/athenia/security/WebSecurityConfig.java
+++ src/main/java/com/athenia/athenia/security/WebSecurityConfig.java
@@ -74,7 +74,7 @@ public class WebSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://ec2-34-227-7-190.compute-1.amazonaws.com", "http://localhost:3000", "http://localhost:8080"));
+		configuration.setAllowedOrigins(Arrays.asList("http://ec2-54-158-114-28.compute-1.amazonaws.com", "http://localhost:3000", "http://localhost:8080"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
 		configuration.setAllowCredentials(true);
