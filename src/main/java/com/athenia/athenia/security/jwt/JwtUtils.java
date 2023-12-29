578e5283a0b8c5b37f18843ef2b0d073002e9951
vvasylykha <vvasylykha@chainfulness.com>
2023-12-29 17:13:55 +0200

    update domain config

--- src/main/java/com/athenia/athenia/security/jwt/JwtUtils.java
+++ src/main/java/com/athenia/athenia/security/jwt/JwtUtils.java
@@ -50,7 +50,7 @@ public class JwtUtils {
 		return ResponseCookie.from(jwtCookie, jwt)
 				.maxAge(MAX_AGE_IN_SECONDS)
 				.sameSite(SAME_SITE)
-				//.domain(DOMAIN)
+				.domain(DOMAIN)
 				.httpOnly(false)
 				.secure(true)
 				.path(PATH)
